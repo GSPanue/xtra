f2dc0deb7c0170468dddee07419808f0d9b6bef6
Gurdev S. Panue <gpanue@hotmail.com>
2019-10-09 22:40:05 +0100

    Configured webpack

---
+++ frontend/config/webpack.base.config.babel.js
@@ -0,0 +1,49 @@
+import VueLoaderPlugin from 'vue-loader/lib/plugin';
+import path from 'path';
+
+const root = path.resolve(__dirname, '..');
+
+const config = {
+  entry: path.join(root, 'src'),
+  output: {
+    path: path.join(root, 'dist/js'),
+    publicPath: '/js/',
+    filename:'bundle.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': path.join(root, 'src'),
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
+
+export default config;
---
+++ frontend/config/webpack.dev.config.babel.js
@@ -0,0 +1,24 @@
+import webpack from 'webpack';
+import path from 'path';
+
+import baseConfig from './webpack.base.config.babel';
+
+const root = path.resolve(__dirname, '..');
+const plugins = baseConfig.plugins;
+
+baseConfig.plugins = [
+  ...plugins,
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoEmitOnErrorsPlugin()
+];
+
+const config = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(root, 'dist'),
+    compress: true
+  },
+  ...baseConfig
+};
+
+export default config;
