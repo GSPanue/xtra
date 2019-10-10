489cd2fe90f9ac96b6db7d4d410a450a32c4e123
Gurdev S. Panue <gpanue@hotmail.com>
2019-10-10 20:51:19 +0100

    Protected home view with guard

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,9 +1,12 @@
 import Route from './Route';
+import Auth from '@/router/Middleware/Auth';
 import Home from '@/views/Home/Index';
+import SignIn from '@/views/SignIn/Index';
 import FourOhFour from '@/views/FourOhFour/Index';
 
 const routes = [
-  Route.set('/', Home, 'home').get(),
+  Route.set('/', Home, 'home').middleware(Auth).get(),
+  Route.set('/signin', SignIn, 'signIn').get(),
   Route.set('*', FourOhFour, 'fourOhFour').get()
 ];
 
