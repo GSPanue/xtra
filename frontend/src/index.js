3dbf656bfd39094d2894b0fca790290a91707e8c
Gurdev S. Panue <gpanue@hotmail.com>
2019-10-10 19:25:58 +0100

    Configured app to use Vue Router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import Vuetify from 'vuetify/lib';
 import App from '@/App';
+import routes from '@/router/routes';
 
+Vue.use(VueRouter);
 Vue.use(Vuetify);
 
 export default new Vue({
   el: '#app',
   components: { App },
+  router: new VueRouter({ mode: 'history', routes }),
   template: '<App/>'
 });
