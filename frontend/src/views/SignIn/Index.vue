72ff6049afcb95ac634c81cb809c4d7b85c7d949
gspanue <gpanue@hotmail.com>
2019-11-09 23:47:49 +0000

    Finished implementing SignIn view

--- frontend/src/views/SignIn/Index.vue
+++ frontend/src/views/SignIn/Index.vue
@@ -7,13 +7,13 @@
         <!-- Sign In Form -->
         <el-form ref="signInForm" :model="signInForm" v-if="!shouldShowRegister" :rules="signInRules" status-icon>
           <el-form-item label="Email Address" prop="emailAddress">
-            <el-input placeholder="Email Address" v-model="signInForm.emailAddress"></el-input>
+            <el-input placeholder="Email Address" v-model="signInForm.emailAddress" :disabled="loading"></el-input>
           </el-form-item>
           <el-form-item label="Password" prop="password">
-            <el-input placeholder="Password" v-model="signInForm.password" show-password></el-input>
+            <el-input placeholder="Password" v-model="signInForm.password" show-password :disabled="loading"></el-input>
           </el-form-item>
-          <el-form-item align="right">
-            <el-button type="primary" @click="submitForm('signInForm')">Sign In</el-button>
+          <el-form-item class="submit-button" align="right">
+            <el-button type="primary" @click="submitForm('signInForm')" :loading="loading">Sign In</el-button>
           </el-form-item>
         </el-form>
 
@@ -22,55 +22,75 @@
           <el-row type="flex">
             <el-col>
               <el-form-item label="First Name" prop="firstName">
-                <el-input placeholder="First Name" v-model="registerForm.firstName"></el-input>
+                <el-input placeholder="First Name" v-model="registerForm.firstName" :disabled="loading"></el-input>
               </el-form-item>
             </el-col>
             <el-col :offset=2>
               <el-form-item label="Last Name" prop="lastName">
-                <el-input placeholder="Last Name" v-model="registerForm.lastName"></el-input>
+                <el-input placeholder="Last Name" v-model="registerForm.lastName" :disabled="loading"></el-input>
               </el-form-item>
             </el-col>
           </el-row>
           <el-row type="flex">
             <el-col>
               <el-form-item label="Email Address" prop="emailAddress">
-                <el-input placeholder="Email Address" v-model="registerForm.emailAddress"></el-input>
+                <el-input placeholder="Email Address" v-model="registerForm.emailAddress" :disabled="loading"></el-input>
               </el-form-item>
             </el-col>
             <el-col :offset=2>
               <el-form-item label="Password" prop="password">
-                <el-input placeholder="Password" v-model="registerForm.password" show-password></el-input>
+                <el-input placeholder="Password" v-model="registerForm.password" show-password :disabled="loading"></el-input>
               </el-form-item>
             </el-col>
           </el-row>
 
           <h4>Account Type</h4>
 
-          <el-row type="flex" justify="space-between">
-            <el-radio v-model="registerForm.accountType" label="1" border>Student</el-radio>
-            <el-radio v-model="registerForm.accountType" label="2" border>Parent</el-radio>
-            <el-radio v-model="registerForm.accountType" label="3" border>Service Provider</el-radio>
+          <el-row type="flex" justify="start">
+            <el-radio v-model="registerForm.accountType" label="Standard" border :disabled="loading">Standard</el-radio>
+            <el-radio v-model="registerForm.accountType" label="Service Provider" border :disabled="loading">Service Provider</el-radio>
           </el-row>
 
-          <el-form-item class="register-button" align="right">
-            <el-button type="primary" @click="submitForm('registerForm')">Register</el-button>
+          <el-form-item class="submit-button" align="right">
+            <el-button type="primary" @click="submitForm('registerForm')" :loading="loading">Register</el-button>
           </el-form-item>
         </el-form>
 
-        <a @click="showRegister" v-if="!shouldShowRegister">Don't have an account? Click here to register!</a>
-        <a @click="showSignIn" v-if="shouldShowRegister">Already have an account? Sign In!</a>
+        <a @click="showRegister" v-if="!shouldShowRegister && !loading">Don't have an account? Click here to register!</a>
+        <a @click="showSignIn" v-if="shouldShowRegister && !loading">Already have an account? Sign In!</a>
     </el-card>
+
+    <!-- Error Message Dialog -->
+    <el-dialog title="Error" :visible.sync="dialogVisible" top="0" width="30%">
+      <span>You have entered an invalid username or password.</span>
+    </el-dialog>
   </el-row>
 </template>
 
 <script>
 import { isEmail, isLength } from 'validator';
+import { getAPIURL } from '@/helpers';
+
+const api = getAPIURL();
 
 export default {
   data() {
     const checkEmailAddress = (rule, value, callback) => {
+      const isRegisterForm = this.shouldShowRegister;
+
       if (!isEmail(value)) {
         callback(new Error('Please enter your email address.'));
+      }
+      else if (isRegisterForm && isEmail(value)) {
+        this.axios.get(`${api}/accounts/find`, {
+          params: {
+            emailAddress: value
+          }
+        }).then(() => {
+          callback(new Error('This email address is already in use.'));
+        }).catch(() => {
+          callback();
+        });
       }
       else {
         callback();
@@ -118,9 +138,11 @@ export default {
         lastName: '',
         emailAddress: '',
         password: '',
-        accountType: '1'
+        accountType: 'Standard'
       },
+      dialogVisible: false,
       shouldShowRegister: false,
+      loading: false,
       signInRules: {
         emailAddress: [
           { validator: checkEmailAddress, trigger: 'blur' }
@@ -149,29 +171,58 @@ export default {
     showRegister() {
       this.shouldShowRegister = true;
 
-      this.signInForm = {
-        emailAddress: '',
-        password: ''
-      };
+      this.$refs['signInForm'].resetFields();
     },
     showSignIn() {
       this.shouldShowRegister = false;
 
-      this.registerForm = {
-        firstName: '',
-        lastName: '',
-        emailAddress: '',
-        password: '',
-        accountType: '1'
-      };
+      this.$refs['registerForm'].resetFields();
+      this.registerForm.accountType = 'Standard';
     },
     submitForm(formName) {
+      this.loading = true;
       const form = this.$refs[formName];
 
       form.validate((valid) => {
+        const isRegisterForm = this.shouldShowRegister;
+
         if (valid) {
-          // Submit form...
-          alert('Valid');
+          if (isRegisterForm) {
+            const account = this.registerForm;
+
+            this.axios.get(`${api}/accounts/create`, {
+              params: {
+                ...account
+              }
+            }).then(() => {
+              this.showSignIn();
+            /**
+             * ToDo: Remove catch method after implementing backend.
+             */
+            }).catch(() => {
+              this.showSignIn();
+            }).finally(() => {
+              this.loading = false;
+            });
+          }
+          else {
+            const account = this.signInForm;
+
+            this.axios.get(`${api}/accounts/login`, {
+              params: {
+                ...account
+              }
+            }).then(() => {
+              this.$router.push('/');
+            }).catch(() => {
+              this.loading = false;
+              this.dialogVisible = true;
+              this.$refs['signInForm'].resetFields();
+            });
+          }
+        }
+        else {
+          this.loading = false;
         }
       });
     }
@@ -189,7 +240,12 @@ export default {
   width: 100%;
 }
 
-.register-button {
+.submit-button {
   padding-top: 22px;
+}
+
+.el-dialog__wrapper {
+  display: flex;
+  align-items: center;
 }
 </style>
