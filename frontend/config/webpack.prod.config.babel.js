37dad619f2d55cb090c1dd164a88220bf1808ab2
gspanue <gpanue@hotmail.com>
2019-11-27 21:25:16 +0000

    Configured sw-precache-webpack-plugin

--- frontend/config/webpack.prod.config.babel.js
+++ frontend/config/webpack.prod.config.babel.js
@@ -1,4 +1,16 @@
 import baseConfig from './webpack.base.config.babel';
+import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
+
+const plugins = baseConfig.plugins;
+
+baseConfig.plugins = [
+  ...plugins,
+  new SWPrecacheWebpackPlugin({
+    cacheId: 'xtra',
+    filename: 'service-worker.js',
+    minify: true
+  })
+];
 
 const config = {
   mode: 'production',
