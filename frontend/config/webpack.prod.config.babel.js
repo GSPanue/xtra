a67939a5e245a80ec39bc07f246aaedf81513320
gspanue <gpanue@hotmail.com>
2019-11-27 23:05:02 +0000

    Replaced w-precache-webpack-plugin with workbox

--- frontend/config/webpack.prod.config.babel.js
+++ frontend/config/webpack.prod.config.babel.js
@@ -1,14 +1,16 @@
 import baseConfig from './webpack.base.config.babel';
-import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
+import { GenerateSW } from 'workbox-webpack-plugin';
+import path from 'path';
 
+const root = path.resolve(__dirname, '..');
 const plugins = baseConfig.plugins;
 
 baseConfig.plugins = [
   ...plugins,
-  new SWPrecacheWebpackPlugin({
-    cacheId: 'xtra',
-    filename: 'service-worker.js',
-    minify: true
+  new GenerateSW({
+    globDirectory: path.join(root, 'dist'),
+    globPatterns: ['**/*.{html,js,css'],
+    swDest: path.join(root, 'dist/service-worker.js')
   })
 ];
 
