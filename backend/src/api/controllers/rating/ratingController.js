8dddf1717fbb106562e65025cf8dd82b320358db
gspanue <gpanue@hotmail.com>
2019-11-28 23:11:39 +0000

    Created rating router and controller

--- backend/src/api/controllers/rating/ratingController.js
+++ backend/src/api/controllers/rating/ratingController.js
@@ -1,5 +1,38 @@
-const dummy = () => {};
+import { getModel } from '@/helpers';
+
+const createRating = ({ body }, res) => {
+  const Rating = getModel('Rating');
+
+  const rating = new Rating({
+    ...body
+  });
+
+  rating.save((error) => {
+    if (error) {
+      res.sendStatus(500);
+    }
+
+    res.sendStatus(200);
+  });
+};
+
+const findRating = ({ query }, res) => {
+  const Rating = getModel('Rating');
+
+  Rating.find({
+    ...query
+  }).then((ratings) => {
+    res.status(200).send({
+      data: ratings
+    });
+  }).catch(() => {
+    res.status(500).send({
+      data: []
+    });
+  });
+};
 
 export {
-  dummy
+  createRating,
+  findRating
 };
--- backend/src/api/routes/rating/ratingRouter.js
+++ backend/src/api/routes/rating/ratingRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
+import { createRating, findRating } from '@/api/controllers';
+
 const ratingRouter = Router();
 
-ratingRouter.get('/', (req, res) => {
-  res.send({});
-});
+ratingRouter.post('/create', createRating);
+ratingRouter.get('/find', findRating);
 
 export default ratingRouter;
