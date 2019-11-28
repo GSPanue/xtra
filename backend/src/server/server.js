463b38b6b779a946e0e59c46d5bb5d4e996c359e
gspanue <gpanue@hotmail.com>
2019-11-28 19:44:40 +0000

    Imported API

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
 
+import api from '@/api';
+
 const app = express();
 const port = process.env.PORT || 8080;
 const staticPath = path.join('..', 'frontend/dist');
 
 app.use(express.static(staticPath));
+app.use('/api/v1', api);
 
 app.get('*', (req, res) => {
   res.sendFile('/frontend/dist/index.html', {
