3d444fbca86662f7dfb9774f69ef4e7c1927d5a8
gspanue <gpanue@hotmail.com>
2019-11-29 20:22:26 +0000

    Implemented listing router and controller

--- backend/src/api/controllers/listing/listingController.js
+++ backend/src/api/controllers/listing/listingController.js
@@ -1,5 +1,73 @@
-const dummy = () => {};
+import { getModel } from '@/helpers';
+
+const createListing = ({ body }, res) => {
+  const Listing = getModel('Listing');
+
+  const listing = new Listing({
+    ...body
+  });
+
+  listing.save((error) => {
+    if (error) {
+      res.sendStatus(500);
+    }
+
+    res.sendStatus(200);
+  });
+};
+
+const updateListing = ({ body }, res) => {
+  const Listing = getModel('Listing');
+  const { _id } = body;
+
+  Listing.findOneAndUpdate({ _id }, body, (error) => {
+    if (error) {
+      res.sendStatus(500);
+    }
+
+    res.sendStatus(200);
+  });
+};
+
+const findListing = ({ query }, res) => {
+  const Listing = getModel('Listing');
+  const isQueryingTopic = (query.topic !== undefined);
+
+  const newQuery = query;
+
+  if (isQueryingTopic) {
+    newQuery.topic = new RegExp(newQuery.topic, 'i');
+  }
+
+  Listing.find({
+    ...newQuery
+  }).then((listings) => {
+    res.status(200).send({
+      data: listings
+    });
+  }).catch(() => {
+    res.status(500).send({
+      data: []
+    });
+  });
+};
+
+const removeListing = ({ body }, res) => {
+  const Listing = getModel('Listing');
+  const { _id } = body;
+
+  Listing.findOneAndRemove({ _id }, (error) => {
+    if (error) {
+      res.sendStatus(500);
+    }
+
+    res.sendStatus(200);
+  });
+};
 
 export {
-  dummy
+  createListing,
+  updateListing,
+  findListing,
+  removeListing
 };
--- backend/src/api/routes/listing/listingRouter.js
+++ backend/src/api/routes/listing/listingRouter.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import {
+  createListing,
+  updateListing,
+  findListing,
+  removeListing
+} from '@/api/controllers';
+
 const listingRouter = Router();
 
-listingRouter.get('/', (req, res) => {
-  res.send({});
-});
+listingRouter.post('/create', createListing);
+listingRouter.put('/update', updateListing);
+listingRouter.get('/find', findListing);
+listingRouter.delete('/remove', removeListing);
 
 export default listingRouter;
