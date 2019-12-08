09eaea10ac91fba045eba99b3ed77b0fdb905dfa
gspanue <gpanue@hotmail.com>
2019-12-08 21:04:41 +0000

    Implemented sign out with axios

--- frontend/src/components/SignOut/Index.vue
+++ frontend/src/components/SignOut/Index.vue
@@ -10,17 +10,24 @@
         <el-button class="cancel-button" @click="handleCancel">Cancel</el-button>
       </el-col>
       <el-col>
-        <el-button class="sign-out-button" type="primary" @click="handleSignOut">Sign Out</el-button>
+        <el-button class="sign-out-button" type="primary" @click="handleSignOut" :loading="isLoggingOut">Sign Out</el-button>
       </el-col>
     </el-row>
   </el-col>
 </template>
 
 <script>
-import store from 'store';
 import { mapMutations } from 'vuex';
+import { getAPIURL } from '@/helpers';
+
+const api = getAPIURL();
 
 export default {
+  data() {
+    return {
+      isLoggingOut: false
+    };
+  },
   methods: {
     ...mapMutations([
       'setRootView',
@@ -40,20 +47,28 @@ export default {
       this.setRootView('home');
     },
     handleSignOut() {
-      this.setAccount(null);
-      this.$router.push('/signin');
+      this.isLoggingOut = true;
 
-      this.setRootView('home');
-      this.setHomeShowInitialView(true);
-      this.setSearchQuery('');
-      this.setSearchResults([]);
-      this.setSortBy('Default');
-      this.setOrder(null);
-      this.setRatingFilter(null);
-      this.setPriceRangeFilter([null, null]);
-      this.setTopicsFilter([]);
-      this.setSearchIsFetching(false);
-      this.setListings([]);
+      this.axios.post(`${api}/account/logout`).then(() => {
+        this.$router.push('/signin', () => {
+          this.setAccount(null);
+          this.setRootView('home');
+          this.setHomeShowInitialView(true);
+          this.setSearchQuery('');
+          this.setSearchResults([]);
+          this.setSortBy('Default');
+          this.setOrder(null);
+          this.setRatingFilter(null);
+          this.setPriceRangeFilter([null, null]);
+          this.setTopicsFilter([]);
+          this.setSearchIsFetching(false);
+          this.setListings([]);
+
+          this.isLoggingOut = false;
+        });
+      }).catch(() => {
+        this.isLoggingOut = false;
+      });
     }
   }
 };
