e34962a0c1e209f4a9f030e5e37d6722dec839f6
gspanue <gpanue@hotmail.com>
2019-12-17 22:52:32 +0000

    Imported Input component

--- native/app/index.js
+++ native/app/index.js
@@ -7,10 +7,13 @@ import store from '@/store';
 import Home from '@/views/Home/Index';
 import SignIn from '@/views/SignIn/Index';
 
+import Input from '@/components/Input/Index';
+
 Vue.use(VueAxios, Axios);
 
 Vue.component('home', Home);
 Vue.component('sign-in', SignIn);
+Vue.component('input', Input);
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
