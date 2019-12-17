02af190bcc442b746ad01d8468e903b9a973ee5a
gspanue <gpanue@hotmail.com>
2019-12-17 22:52:20 +0000

    Created Input component

---
+++ native/app/components/Input/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <FlexboxLayout class="input" flexDirection="column">
+    <Label v-if="label" class="label" :text="label" />
+    <TextField
+      class="text-field"
+      :keyboardType="keyboardType"
+      :secure="secure"
+    />
+  </FlexboxLayout>
+</template>
+
+<script>
+export default {
+  props: [
+    'label',
+    'keyboardType',
+    'secure'
+  ]
+}
+</script>
+
+<style scoped>
+.input {
+  margin-top: 40px;
+}
+
+.label {
+  font-size: 22px;
+}
+
+.text-field {
+  font-size: 18px;
+  margin: 0;
+}
+</style>
