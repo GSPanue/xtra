c3fdade0b3754868c6a3d34e7cb4990be741b6cd
gspanue <gpanue@hotmail.com>
2019-12-17 20:26:28 +0000

    Added account and listings state

--- native/app/store/store.js
+++ native/app/store/store.js
@@ -4,9 +4,26 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {}
+  state: {
+    account: null,
+    listings: []
+  },
+  getters: {
+    getAccount: ({ account }) => (
+      account
+    ),
+    getListings: ({ listings }) => (
+      listings
+    )
+  },
+  mutations: {
+    setAccount: (store, newAccount) => {
+      store.account = newAccount;
+    },
+    setListings: (store, newListings) => {
+      store.listings = newListings
+    }
+  }
 });
 
 export default store;
