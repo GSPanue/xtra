ef0ba84d687d588f42c27f5bad79d649598b49ba
gspanue <gpanue@hotmail.com>
2019-12-17 17:13:07 +0000

    Imported views

--- native/app/index.js
+++ native/app/index.js
@@ -2,6 +2,12 @@ import Vue from 'nativescript-vue'
 import App from '@/App'
 import store from '@/store';
 
+import Home from '@/views/Home/Index';
+import SignIn from '@/views/SignIn/Index';
+
+Vue.component('home', Home);
+Vue.component('sign-in', SignIn);
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
