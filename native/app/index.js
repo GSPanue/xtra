a61f7270b6f2f8c0430fc1328f9031adbf569a7c
gspanue <gpanue@hotmail.com>
2019-12-17 17:13:55 +0000

    Imported axios

--- native/app/index.js
+++ native/app/index.js
@@ -1,9 +1,13 @@
 import Vue from 'nativescript-vue'
+import Axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from '@/App'
 import store from '@/store';
 
 import Home from '@/views/Home/Index';
 import SignIn from '@/views/SignIn/Index';
+
+Vue.use(VueAxios, Axios);
 
 Vue.component('home', Home);
 Vue.component('sign-in', SignIn);
