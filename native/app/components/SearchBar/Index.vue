a8d175e7684e38a86c08ff2fa90e2f709c09f3e5
gspanue <gpanue@hotmail.com>
2019-12-20 20:59:32 +0000

    Implemented search bar

--- native/app/components/SearchBar/Index.vue
+++ native/app/components/SearchBar/Index.vue
@@ -1,3 +1,58 @@
 <template>
-  <Label text="Search Bar" />
+  <FlexboxLayout flexDirection="row">
+    <SearchBar
+      ref="searchBar"
+      class="search-bar"
+      hint="Search for a class or activity..."
+      @submit="handleSubmit"
+      @clear="clearFocus"
+      @loaded="clearFocus"
+    />
+  </FlexboxLayout>
 </template>
+
+<script>
+import { mapMutations } from 'vuex';
+import { getAPIURL } from '@/helpers';
+
+const api = getAPIURL();
+
+export default {
+  methods: {
+    ...mapMutations([
+      'setIsBusy',
+      'setSearchQuery',
+      'setResults'
+    ]),
+    clearFocus(args) {
+      const searchBar = args.object;
+
+      searchBar.android.clearFocus();
+    },
+    handleSubmit(args) {
+      const query = args.object.text;
+      this.clearFocus(args);
+      this.setIsBusy(true);
+
+      this.axios.get(`${api}/listing/find`, {
+        params: {
+          topic: query
+        }
+      }).then(({ data: results }) => {
+        this.setSearchQuery(query);
+        this.setResults(results);
+
+        this.setIsBusy(false);
+      }).catch(() => {
+        this.setIsBusy(false);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-bar {
+  font-size: 16px;
+}
+</style>
