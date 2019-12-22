95808f15bf1e6b194540a648cc54ef3823003616
gspanue <gpanue@hotmail.com>
2019-12-22 20:17:28 +0000

    Added topic and price columns

--- native/app/components/Listing/Index.vue
+++ native/app/components/Listing/Index.vue
@@ -1,3 +1,19 @@
 <template>
-  <Label text="Listing" />
+  <FlexboxLayout flexDirection="column">
+    <FlexboxLayout flexDirection="row" backgroundColor="red">
+      <!-- Topic & Price -->
+      <FlexboxLayout class="flex-1" flexDirection="column" alignItems="flex-start">
+        <Label text="Topic" />
+      </FlexboxLayout>
+      <FlexboxLayout class="flex-1" flexDirection="column" alignItems="flex-end">
+        <Label text="Price" />
+      </FlexboxLayout>
+    </FlexboxLayout>
+  </FlexboxLayout>
 </template>
+
+<style scoped>
+.flex-1 {
+  flex: 1;
+}
+</style>
